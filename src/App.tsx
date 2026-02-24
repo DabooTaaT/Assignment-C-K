7d894b4c11cf7c5e6360daca779053074126dc09
Thanapat.K <baac-dbm-dev@scbtechx.io>
2026-02-25 00:55:37 +0700

    feat(app): wrap with MUI ThemeProvider and CssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+// src/App.tsx
 import { RouterProvider } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client/react";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { apolloClient } from "@/core/network";
 import { router } from "@/core/router";
+import { muiTheme } from "@/core/theme";
 import "@/core/i18n";
 
 function App() {
   return (
     <ApolloProvider client={apolloClient}>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={muiTheme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </ApolloProvider>
   );
 }
