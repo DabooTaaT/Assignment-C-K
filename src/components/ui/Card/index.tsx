2223da3dad9045aec6aa0c2f23e31fb4aaa904a5
Thanapat.K <baac-dbm-dev@scbtechx.io>
2026-02-25 01:08:04 +0700

    fix(ui/Card,Table): add forwardRef to Card wrappers, add missing Table prop type exports

--- src/components/ui/Card/index.tsx
+++ src/components/ui/Card/index.tsx
@@ -1,10 +1,26 @@
 // src/components/ui/Card/index.tsx
+import React from "react";
 import MuiCard, { type CardProps } from "@mui/material/Card";
 import MuiCardContent, { type CardContentProps } from "@mui/material/CardContent";
 import MuiCardHeader, { type CardHeaderProps } from "@mui/material/CardHeader";
 import MuiCardActions, { type CardActionsProps } from "@mui/material/CardActions";
 
-export const Card = (props: CardProps) => <MuiCard {...props} />;
-export const CardContent = (props: CardContentProps) => <MuiCardContent {...props} />;
-export const CardHeader = (props: CardHeaderProps) => <MuiCardHeader {...props} />;
-export const CardActions = (props: CardActionsProps) => <MuiCardActions {...props} />;
+export const Card = React.forwardRef<HTMLDivElement, CardProps>((props, ref) => (
+  <MuiCard ref={ref} {...props} />
+));
+Card.displayName = "Card";
+
+export const CardContent = React.forwardRef<HTMLDivElement, CardContentProps>((props, ref) => (
+  <MuiCardContent ref={ref} {...props} />
+));
+CardContent.displayName = "CardContent";
+
+export const CardHeader = React.forwardRef<HTMLDivElement, CardHeaderProps>((props, ref) => (
+  <MuiCardHeader ref={ref} {...props} />
+));
+CardHeader.displayName = "CardHeader";
+
+export const CardActions = React.forwardRef<HTMLDivElement, CardActionsProps>((props, ref) => (
+  <MuiCardActions ref={ref} {...props} />
+));
+CardActions.displayName = "CardActions";
--- src/components/ui/Table/index.tsx
+++ src/components/ui/Table/index.tsx
@@ -3,6 +3,8 @@ export { default as Table } from "@mui/material/Table";
 export type { TableProps } from "@mui/material/Table";
 
 export { default as TableBody } from "@mui/material/TableBody";
+export type { TableBodyProps } from "@mui/material/TableBody";
+
 export { default as TableCell } from "@mui/material/TableCell";
 export type { TableCellProps } from "@mui/material/TableCell";
 
@@ -10,5 +12,10 @@ export { default as TableContainer } from "@mui/material/TableContainer";
 export type { TableContainerProps } from "@mui/material/TableContainer";
 
 export { default as TableHead } from "@mui/material/TableHead";
+export type { TableHeadProps } from "@mui/material/TableHead";
+
 export { default as TableRow } from "@mui/material/TableRow";
+export type { TableRowProps } from "@mui/material/TableRow";
+
 export { default as TableFooter } from "@mui/material/TableFooter";
+export type { TableFooterProps } from "@mui/material/TableFooter";
