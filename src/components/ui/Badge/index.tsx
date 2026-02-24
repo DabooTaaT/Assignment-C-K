3d4722ee81dbe9cb50bf7fcbf3579ee851c3b20e
Thanapat.K <baac-dbm-dev@scbtechx.io>
2026-02-25 01:12:02 +0700

    fix(ui/Badge): add forwardRef consistent with Card pattern

--- src/components/ui/Badge/index.tsx
+++ src/components/ui/Badge/index.tsx
@@ -1,6 +1,10 @@
 // src/components/ui/Badge/index.tsx
+import React from "react";
 import MuiChip, { type ChipProps } from "@mui/material/Chip";
 
 export type BadgeProps = ChipProps;
 
-export const Badge = (props: ChipProps) => <MuiChip {...props} />;
+export const Badge = React.forwardRef<HTMLDivElement, BadgeProps>((props, ref) => (
+  <MuiChip ref={ref} {...props} />
+));
+Badge.displayName = "Badge";
