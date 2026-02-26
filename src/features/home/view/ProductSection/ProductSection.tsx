01e2144029711e9354e205eb8695e27685c6cc30
Thanapat.K <baac-dbm-dev@scbtechx.io>
2026-02-27 00:47:38 +0700

    docs: update README.md and enhance ProductSection styling
    - Removed outdated content from README.md and replaced it with a brief note.
    - Enhanced the ProductSection component by adding a background color and padding for improved visual presentation.

--- src/features/home/view/ProductSection/ProductSection.tsx
+++ src/features/home/view/ProductSection/ProductSection.tsx
@@ -9,15 +9,15 @@ interface ProductSectionProps {
 export const ProductSection = ({ products }: ProductSectionProps) => {
   const { t } = useTranslation("home");
   return (
-  <section id="products">
-    <h2 className="text-[#3e7ced] text-center my-10 text-[28px] font-bold uppercase">
-      {t("sections.product")}
-    </h2>
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 max-w-[1200px] mx-auto mb-12 px-5">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
-  </section>
+    <section id="products" className="bg-[#dbeafe] py-8 px-5">
+      <h2 className="text-[#3e7ced] text-center my-10 text-[28px] font-bold uppercase">
+        {t("sections.product")}
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 max-w-[1200px] mx-auto mb-12 px-5">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </section>
   );
 };
