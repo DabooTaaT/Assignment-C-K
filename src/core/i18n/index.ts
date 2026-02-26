aa5ea7735b250972280b7a329e3e423c0a22d4d7
Thanapat.K <baac-dbm-dev@scbtechx.io>
2026-02-27 00:22:50 +0700

    feat(i18n): add enhanceHtml namespace with en/th translations

--- src/core/i18n/index.ts
+++ src/core/i18n/index.ts
@@ -5,9 +5,11 @@ import * as LanguageDetectorModule from "i18next-browser-languagedetector";
 import enCommon from "./locales/en/common.json";
 import enAuth from "./locales/en/auth.json";
 import enHome from "./locales/en/home.json";
+import enEnhanceHtml from "./locales/en/enhanceHtml.json";
 import thCommon from "./locales/th/common.json";
 import thAuth from "./locales/th/auth.json";
 import thHome from "./locales/th/home.json";
+import thEnhanceHtml from "./locales/th/enhanceHtml.json";
 
 const i18n = createInstance();
 const LanguageDetector =
@@ -22,11 +24,13 @@ i18n.use(LanguageDetector).use(initReactI18next).init({
       common: unwrap(enCommon),
       auth: unwrap(enAuth),
       home: unwrap(enHome),
+      enhanceHtml: unwrap(enEnhanceHtml),
     },
     th: {
       common: unwrap(thCommon),
       auth: unwrap(thAuth),
       home: unwrap(thHome),
+      enhanceHtml: unwrap(thEnhanceHtml),
     },
   },
   fallbackLng: "en",
